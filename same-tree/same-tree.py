0e3002ffd2c4a3080575422d6889c81e706f4051
Tyler Guo <tylereguo@gmail.com>
2020-12-04 14:52:57 -0800

    working commit - Created using LeetHub

---
+++ same-tree/same-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        stack = [(p,q)]
+        
+        while stack:
+            l, r = stack.pop()
+            
+            if not l and not r:
+                continue
+            
+            if not l or not r:
+                return False
+            
+            if l.val != r.val:
+                return False
+            
+            stack.append((l.left, r.left))
+            stack.append((l.right, r.right))
+            
+        return True
+        
