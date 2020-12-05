10fe87ab8003577438f0c9298dc652f6bf917880
Tyler Guo <tylereguo@gmail.com>
2020-12-04 19:47:03 -0800

    Update subsets-ii.py

--- subsets-ii/subsets-ii.py
+++ subsets-ii/subsets-ii.py
@@ -1,10 +1,17 @@
 class Solution:
-    def subsetsWithDup(self, arr: List[int]) -> List[List[int]]:
-        arr = sorted(arr)
-        subsets = [[]]
-        for num in arr:
-            for i in range(len(subsets)):
-                curr = subsets[i]
-                if curr + [num] not in subsets:
-                    subsets.append(curr + [num])
-        return subsets
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
+        output = []
+        def backtrack(first = 0, cur = []):
+            output.append(cur[:])
+            for i in range(first, n):
+				#in case same combination is used many times
+                if i>first and nums[i] == nums[i-1]:
+                    continue
+                else:
+                    cur.append(nums[i])
+                    backtrack(i+1, cur)
+                    cur.pop()
+        backtrack()
+        return output
