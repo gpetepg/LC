2f59f0ed3d22d7307bed5ae6584f566232aaf99f
Tyler Guo <tylereguo@gmail.com>
2021-01-19 18:04:51 -0800

    Time: 144 ms (56.51%), Space: 15.6 MB (37.63%) - LeetHub

--- number-of-islands/number-of-islands.py
+++ number-of-islands/number-of-islands.py
@@ -1,29 +1,33 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        def dfs(row, col):
-            
-            grid[row][col] = "-1"
+        def dfs(r, c):
+            # mark the island
+            grid[r][c] = "-1"
             
             neighbors = [
-                (row + 1, col),
-                (row - 1, col),
-                (row, col + 1),
-                (row, col - 1)
+                (r - 1, c),
+                (r + 1, c),
+                (r, c - 1),
+                (r, c + 1)
             ]
             
-            for r, c in neighbors:
-                if r >= 0 and c >= 0 and r < len(grid) and c < len(grid[row]) and grid[r][c] == "1":
-                    dfs(r, c)
-            
-            
-        island_count = 0
-            
+            for _r, _c in neighbors:
+                if _r >= 0 and _c >= 0 and _r < len(grid) and _c < len(grid[r]) and grid[_r][_c] == "1":
+                    dfs(_r, _c)
         
-        for row in range(len(grid)):
-            for col in range(len(grid[row])):
+        
+        
+        # number of islands
+        number_of_islands = 0
+        
+        # iterate through the matrix
+        for row, rv in enumerate(grid):
+            for col, cv in enumerate(rv):
                 if grid[row][col] == "1":
-                    island_count += 1
+                    number_of_islands += 1
                     dfs(row, col)
-            
-        return island_count
+                
+        return number_of_islands
+                    
+​
