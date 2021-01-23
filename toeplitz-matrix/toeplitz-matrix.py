208195c35658381d31be86b026d38f04e22b9578
Tyler Guo <tylereguo@gmail.com>
2021-01-23 11:41:55 -0800

    Time: 88 ms (52.91%), Space: 14.4 MB (7.00%) - LeetHub

--- toeplitz-matrix/toeplitz-matrix.py
+++ toeplitz-matrix/toeplitz-matrix.py
@@ -1,8 +1,8 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        for row, v in enumerate(matrix):
-            for col, _v  in enumerate(v):
+        for row in range(len(matrix)):
+            for col in range(len(matrix[row])):
                 if (row == 0 or col == 0 or matrix[row][col] == matrix[row - 1][col - 1]) == False:
                     return False
         return True
-        
+    
